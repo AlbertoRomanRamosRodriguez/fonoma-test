Validate status and price. Fix amount calculation
Added a constant for valid statuses and changed the endpoint params

main.py
@@ -1,13 +1,15 @@
 
 from pydantic import BaseModel
-from typing import List
-from fastapi import FastAPI, Response, status
+from typing import List, Annotated
+from fastapi import FastAPI, Body, status
 
 import numpy as np
 
 app = FastAPI()
 
-VALID_CRITERIONS = ['completed', 'pending', 'canceled', 'all']
+VALID_STATUSES = ['completed', 'pending', 'canceled']
+VALID_CRITERIONS = VALID_STATUSES
+VALID_CRITERIONS.append('all')
 
 class Order(BaseModel):
     id: int
@@ -16,28 +18,22 @@ class Order(BaseModel):
     price: float
     status: str
 
-class OrderData(BaseModel):
-    orders : List[Order]
-    criterion: str
-
 @app.post("/solution", status_code=status.HTTP_200_OK)
-def process_orders(order_data:OrderData, response:Response) -> float:
-    
-    orders = order_data.orders
-    criterion = order_data.criterion
+def process_orders(orders:List[Order], criterion:Annotated[str, Body()]) -> float:
 
     total = 0
 
-    if not criterion in VALID_CRITERIONS:
-        response.status_code = status.HTTP_406_NOT_ACCEPTABLE
+    negative_prices = [o.price < 0 for o in orders] # check for negative prices
+    invalid_statuses = [o.status not in VALID_STATUSES for o in orders] # check for invalid statuses
 
+    if any(negative_prices) or any(invalid_statuses) or not criterion in VALID_CRITERIONS:
         return -1
 
     if criterion == 'all':
-        prices = np.array([order.price for order in orders])
+        amounts = np.array([o.price * o.quantity for o in orders])
     else:
-        prices = np.array([order.price for order in orders if order.status == criterion])
+        amounts = np.array([o.price * o.quantity for o in orders if o.status == criterion])
         
-    total = np.sum(prices, where=(prices>=0))
+    total = np.sum(amounts)
 
     return total